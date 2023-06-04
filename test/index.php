e7f95d4dba995e3843dfc43cb7ed69bc4cb8c156
Mahmoud <eng.mahmoud.alali@gmail.com>
2023-06-04 16:43:57 +0400

    GIT

--- test/index.php
+++ test/index.php
@@ -1,10 +1,20 @@
 <?php
 
-$name = "Mahmoud";
-$age = 12;
+// $name = "Mahmoud";
+// $age = 12;
 
- echo "Are you". " "  . "$age ".  $name; 
+//  echo "Are you". " "  . "$age ".  $name; 
 
 
+// GET the variables is public put on links But when i ues POST this is very securty
+$name = $_GET["name"];
+$age = $_GET["age"];
+$email = $_GET["email"];
+
+echo $name;
+echo "<br/>";
+echo $age;
+echo "<br/>";
+echo $email;
 ?>
     
