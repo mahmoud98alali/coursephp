826a8990d040531dd6e6d425794b6853ee26b988
Mahmoud <eng.mahmoud.alali@gmail.com>
2023-06-11 23:22:51 +0400

    post

--- test/index.php
+++ test/index.php
@@ -4,17 +4,34 @@
 // $age = 12;
 
 //  echo "Are you". " "  . "$age ".  $name; 
+//-------------------------------------------------------------------------------------------
 
+// GET the variables is public put on links But when i ues POST this is very securty so the varavles i put in body
+// $name = $_GET["name"];
+// $age = $_GET["age"];
+// $email = $_GET["email"];
 
-// GET the variables is public put on links But when i ues POST this is very securty
-$name = $_GET["name"];
-$age = $_GET["age"];
-$email = $_GET["email"];
+// echo $name;
+// echo "<br/>";
+// echo $age;
+// echo "<br/>";
+// echo $email;
+// echo "<br/>";
+// print_r($_GET);
+//-------------------------------------------------------------------------------------------
+// Request POST
+
+$name = $_POST['name'];
+$age1 = $_POST['age2'];
+$email = $_POST['email'];
 
 echo $name;
-echo "<br/>";
-echo $age;
-echo "<br/>";
+// echo "<br/>";
+echo $age1;
+// echo "<br/>";
 echo $email;
+// echo "<br/>";
+// print_r($_POST);
+//-------------------------------------------------------------------------------------------
 ?>
     
